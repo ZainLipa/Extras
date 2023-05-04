7c6515c3e9795fc21c452dc01529cca07f706de1
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-05-04 23:06:29 +0500

    Gcd program

---
+++ Gcd.py
@@ -0,0 +1,9 @@
+def gcd(a, b):
+    """
+    Calculates the greatest common divisor (gcd) of two integers using the Euclidean algorithm.
+    """
+    while b:
+        a, b = b, a % b
+    return a
+
+print(gcd(5,4))
