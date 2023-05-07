4496b64163ed3e8d0b4f7f3f6cb7585d5363f414
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-05-07 20:01:07 +0500

    Create Integral.py

---
+++ Integral.py
@@ -0,0 +1,15 @@
+from scipy.integrate import quad
+
+# Define the function to be integrated
+def integrand(x):
+    return x**2
+
+# Define the limits of integration
+a = 0
+b = 1
+
+# Call the quad function to perform the integration
+result, error = quad(integrand, a, b)
+
+# Print the result
+print(f"The integral of x^2 from {a} to {b} is {result:.3f}")
