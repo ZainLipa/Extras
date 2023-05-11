046972c921ff1e76f43d0241f28567f0c71c5554
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-05-11 20:50:27 +0500

    Create Guess_the_number.py

---
+++ Guess_the_number.py
@@ -0,0 +1,16 @@
+import random
+
+number = random.randint(1, 100)
+
+print("I'm thinking of a number between 1 and 100. Can you guess what it is?")
+
+while True:
+    guess = int(input("Enter your guess: "))
+    
+    if guess < number:
+        print("Too low! Guess again.")
+    elif guess > number:
+        print("Too high! Guess again.")
+    else:
+        print("Congratulations! You guessed the number!")
+        break
