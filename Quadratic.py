295d300ac84f1673bf60d547c445ac1949373dc5
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-05-14 17:14:04 +0500

    Create Quadratic.py

---
+++ Quadratic.py
@@ -0,0 +1,18 @@
+import cmath
+
+def quadratic_roots(a, b, c):
+    # calculate the discriminant
+    d = (b**2) - (4*a*c)
+
+    # calculate the roots
+    root1 = (-b + cmath.sqrt(d)) / (2*a)
+    root2 = (-b - cmath.sqrt(d)) / (2*a)
+
+    return root1, root2
+
+a = float(input("Enter the coefficient of x^2: "))
+b = float(input("Enter the coefficient of x: "))
+c = float(input("Enter the constant term: "))
+
+root1, root2 = quadratic_roots(a, b, c)
+print("The roots of the quadratic equation are:", root1, "and", root2)
