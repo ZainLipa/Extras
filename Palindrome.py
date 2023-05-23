0bdc88111b774283c71cb125f189badf6f655236
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-05-23 20:09:43 +0500

    Create Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,12 @@
+def is_palindrome(string):
+    # Remove whitespace and convert to lowercase
+    string = string.replace(" ", "").lower()
+    # Check if the string is equal to its reverse
+    return string == string[::-1]
+
+string = input("Enter a string: ")
+
+if is_palindrome(string):
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
