05db8cbc8aee143154d3911fb62ebf1caca7c77e
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-05-26 23:34:43 +0500

    Create Recursive_factorial.py

---
+++ Recursive_factorial.py
@@ -0,0 +1,13 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+num = int(input("Enter a non-negative integer: "))
+
+if num < 0:
+    print("Error: Please enter a non-negative integer.")
+else:
+    fact = factorial(num)
+    print("The factorial of", num, "is", fact)
