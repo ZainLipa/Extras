42eaff5d75abe239a7abd9c8b66daca07966a76e
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-05-31 19:46:04 +0500

    Create Average.py

---
+++ Average.py
@@ -0,0 +1,17 @@
+def calculate_average(numbers):
+    if len(numbers) == 0:
+        return 0
+    total = sum(numbers)
+    average = total / len(numbers)
+    return average
+
+# Prompt the user to enter a list of numbers separated by spaces
+number_list = input("Enter a list of numbers separated by spaces: ").split()
+
+# Convert the input numbers to integers
+numbers = [int(num) for num in number_list]
+
+# Calculate the average of the numbers
+average = calculate_average(numbers)
+
+print("Average:", average)
