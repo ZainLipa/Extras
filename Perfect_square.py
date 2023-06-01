9281a9680a858fe05a4021d4b5f2e31619817767
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-06-01 21:27:53 +0500

    Create Perfect_square.py

---
+++ Perfect_square.py
@@ -0,0 +1,10 @@
+def is_perfect_square(num):
+    square_root = int(num ** 0.5)
+    return square_root * square_root == num
+
+num = int(input("Enter a positive integer: "))
+
+if is_perfect_square(num):
+    print(num, "is a perfect square.")
+else:
+    print(num, "is not a perfect square.")
