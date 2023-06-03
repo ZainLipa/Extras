d0092282bb7cb3b7ec61050bad3b20aa38d9a689
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-06-03 20:36:37 +0500

    Create Multiples.py

---
+++ Multiples.py
@@ -0,0 +1,12 @@
+def calculate_multiples_sum(n):
+    total = 0
+    for i in range(n):
+        if i % 3 == 0 or i % 5 == 0:
+            total += i
+    return total
+
+number = int(input("Enter a number: "))
+
+sum_of_multiples = calculate_multiples_sum(number)
+
+print("Sum of multiples of 3 or 5 below", number, "is:", sum_of_multiples)
