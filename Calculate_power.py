e111d2d68d9171c0e01eb918966476d1cd9956d9
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-06-10 10:53:19 +0500

    Create Calculate_power.py

---
+++ Calculate_power.py
@@ -0,0 +1,12 @@
+def calculate_power(base, exponent):
+    result = 1
+    for _ in range(exponent):
+        result *= base
+    return result
+
+base = int(input("Enter the base: "))
+exponent = int(input("Enter the exponent: "))
+
+power = calculate_power(base, exponent)
+
+print(base, "raised to the power of", exponent, "is:", power)
