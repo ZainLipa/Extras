5b4cf11d47515ab3e0efe3add18e68029fcde362
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-06-11 13:11:13 +0500

    Create Extreme_numbers.py

---
+++ Extreme_numbers.py
@@ -0,0 +1,23 @@
+def find_extremes(numbers):
+    if len(numbers) == 0:
+        return None, None
+    smallest = largest = numbers[0]
+    for num in numbers:
+        if num < smallest:
+            smallest = num
+        if num > largest:
+            largest = num
+    return smallest, largest
+
+number_list = input("Enter a list of numbers separated by spaces: ").split()
+
+# Convert the input numbers to integers
+numbers = [int(num) for num in number_list]
+
+smallest, largest = find_extremes(numbers)
+
+if smallest is not None and largest is not None:
+    print("Smallest number:", smallest)
+    print("Largest number:", largest)
+else:
+    print("No numbers were provided.")
