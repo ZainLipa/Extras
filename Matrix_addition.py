54b49fdd920d708670259f3364db62e479240bf3
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-06-12 20:44:20 +0500

    Create Matrix_addition.py

---
+++ Matrix_addition.py
@@ -0,0 +1,18 @@
+def matrix_addition(matrix1, matrix2):
+    result = []
+    for i in range(len(matrix1)):
+        row = []
+        for j in range(len(matrix1[i])):
+            row.append(matrix1[i][j] + matrix2[i][j])
+        result.append(row)
+    return result
+
+# Example matrices for demonstration
+matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+matrix2 = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
+
+result = matrix_addition(matrix1, matrix2)
+
+print("Matrix Addition Result:")
+for row in result:
+    print(row)
