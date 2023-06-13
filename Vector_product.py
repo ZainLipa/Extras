2712d9989f98c7a8c3fa70a5f3c19703bbfe4a15
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-06-13 19:56:34 +0500

    Create Vector_product.py

---
+++ Vector_product.py
@@ -0,0 +1,13 @@
+def dot_product(vector1, vector2):
+    if len(vector1) != len(vector2):
+        return None
+    result = sum(x * y for x, y in zip(vector1, vector2))
+    return result
+
+# Example vectors for demonstration
+vector1 = [1, 2, 3]
+vector2 = [4, 5, 6]
+
+dot_product_result = dot_product(vector1, vector2)
+
+print("Dot Product Result:", dot_product_result)
