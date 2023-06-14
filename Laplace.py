04f55b60a02c35ba4902dbb6f4f2395a35a05dc7
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-06-14 22:11:38 +0500

    Create Laplace.py

---
+++ Laplace.py
@@ -0,0 +1,20 @@
+def determinant(matrix):
+    n = len(matrix)
+    if n == 1:
+        return matrix[0][0]
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for j in range(n):
+        submatrix = [[matrix[i][k] for k in range(n) if k != j] for i in range(1, n)]
+        det += (-1) ** j * matrix[0][j] * determinant(submatrix)
+
+    return det
+
+# Example matrix for demonstration
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+det = determinant(matrix)
+
+print("Determinant:", det)
