ee254aced983ea165ecd265c7ffc3778826d5400
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-06-17 14:53:46 +0500

    Create Largest_prime_factor.py

---
+++ Largest_prime_factor.py
@@ -0,0 +1,15 @@
+def largest_prime_factor(number):
+    factor = 2
+    while factor * factor <= number:
+        if number % factor == 0:
+            number //= factor
+        else:
+            factor += 1
+    if number > 1:
+        return number
+    return factor
+
+# Example usage
+number = int(input("Enter a number: "))
+largest_prime = largest_prime_factor(number)
+print("Largest Prime Factor:", largest_prime)
