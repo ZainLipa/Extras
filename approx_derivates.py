2368ecbc25b8d6a8eb4fbbdf6715ebbb5eb1e4cd
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-06-18 18:43:54 +0500

    Create approx_derivates.py

---
+++ approx_derivates.py
@@ -0,0 +1,12 @@
+def derivative(f, x, h):
+    return (f(x + h) - f(x - h)) / (2 * h)
+
+# Example usage
+def f(x):
+    return x**2  # Define your function here
+
+x = 2  # Point at which to approximate the derivative
+h = 0.001  # Step size
+
+approx_derivative = derivative(f, x, h)
+print("Approximate Derivative:", approx_derivative)
