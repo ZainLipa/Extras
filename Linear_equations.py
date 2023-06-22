dbea12095f7e6d9025b42083bd3bd9bab82f1743
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-06-22 20:12:07 +0500

    Create Linear_equations.py

---
+++ Linear_equations.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def solve_linear_equations(coefficients, constants):
+    A = np.array(coefficients)
+    b = np.array(constants)
+    
+    try:
+        solution = np.linalg.solve(A, b)
+        return solution.tolist()
+    except np.linalg.LinAlgError:
+        return None
+
+# Example usage
+coefficients = [[2, 1, -1], [1, -
