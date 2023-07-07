6fe04cd687fd2933a3202342a1ae74fce3e0252c
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-07-07 12:54:12 +0500

    Create BFS.py

---
+++ BFS.py
@@ -0,0 +1,28 @@
+# Program of the Day: Breadth-First Search (BFS)
+
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        vertex = queue.popleft()
+        if vertex not in visited:
+            print(vertex)
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+
+# Example usage
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F'},
+    'D': {'B'},
+    'E': {'B', 'F'},
+    'F': {'C', 'E'}
+}
+
+start_vertex = 'A'
+print("BFS traversal:")
+bfs(graph, start_vertex)
