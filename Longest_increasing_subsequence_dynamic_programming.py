64d7ff68f7ce8a572c926a0b5d265db637770d64
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-08-02 20:53:08 +0500

    Create Longest_increasing_subsequence_dynamic_programming.py

---
+++ Longest_increasing_subsequence_dynamic_programming.py
@@ -0,0 +1,20 @@
+# Program of the Day: Longest Increasing Subsequence (Dynamic Programming)
+
+def longest_increasing_subsequence(nums):
+    if not nums:
+        return 0
+
+    n = len(nums)
+    dp = [1] * n
+
+    for i in range(1, n):
+        for j in range(i):
+            if nums[i] > nums[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return max(dp)
+
+# Example usage
+arr = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+result = longest_increasing_subsequence(arr)
+print("Length of the Longest Increasing Subsequence:", result)
