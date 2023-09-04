9646abfc0155a211ac6fbe66e469c74df4922fa8
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-09-04 21:23:56 +0500

    Create Merge_sort.py

---
+++ Merge_sort.py
@@ -0,0 +1,37 @@
+# Program of the Day: Merge Sort Algorithm
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+# Example usage
+unsorted_array = [3, 6, 8, 10, 1, 2, 1]
+sorted_array = merge_sort(unsorted_array)
+
+print("Unsorted Array:", unsorted_array)
+print
