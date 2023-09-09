3f1dd1d9967fa6a9c4cc1fba09a9f50b163c2597
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-09-09 19:32:57 +0500

    Create Adventure_game.py

---
+++ Adventure_game.py
@@ -0,0 +1,23 @@
+# Program of the Day: Text-Based Adventure Game
+
+def start_game():
+    print("Welcome to the Text-Based Adventure Game!")
+    print("You find yourself in a mysterious forest. You can go north or south.")
+
+    while True:
+        choice = input("What direction do you choose? (north/south/quit): ").lower()
+
+        if choice == "north":
+            print("You venture deeper into the forest and discover a hidden treasure!")
+            print("Congratulations, you win!")
+            break
+        elif choice == "south":
+            print("You encounter a wild bear and run away in fear.")
+        elif choice == "quit":
+            print("Thanks for playing!")
+            break
+        else:
+            print("Invalid choice. Please enter 'north', 'south', or 'quit'.")
+
+# Start the game
+start_game()
