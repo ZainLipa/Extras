0747a841bce9affc0eef2797ae17c350ae7b9e2f
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-10-03 20:55:37 +0500

    Create Dice_roller.py

---
+++ Dice_roller.py
@@ -0,0 +1,16 @@
+# Program of the Day: Dice Roller
+
+import random
+
+def roll_dice(num_dice, num_sides):
+    rolls = [random.randint(1, num_sides) for _ in range(num_dice)]
+    return rolls
+
+# Example usage: Roll two six-sided dice
+num_dice = 2
+num_sides = 6
+
+dice_rolls = roll_dice(num_dice, num_sides)
+print(f"Rolling {num_dice} {num_sides}-sided dice:")
+print("Result:", dice_rolls)
+print("Total:", sum(dice_rolls))
