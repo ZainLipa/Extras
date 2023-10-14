f8fa4ca298b21f7ec1011eaf60614c3c6ec6c1e2
ZainLipa <57558837+ZainLipa@users.noreply.github.com>
2023-10-14 18:19:40 +0500

    Create reverse_text.py

---
+++ reverse_text.py
@@ -0,0 +1,10 @@
+# Program of the Day: Text Reverser
+
+def reverse_text(text):
+    return text[::-1]
+
+# Example usage: Reverse a text
+original_text = "Hello, World!"  # You can change this text
+reversed_text = reverse_text(original_text)
+print("Original Text: " + original_text)
+print("Reversed Text: " + reversed_text)
